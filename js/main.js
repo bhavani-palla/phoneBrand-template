5694ab7be078f8e0e2396bf4303e6a74ec596235
Bhavani Palla <bhavani.palla@gmail.com>
2021-11-27 09:04:57 +0100

    Added name and image functionality

--- js/main.js
+++ js/main.js
@@ -3,31 +3,57 @@ const inputPhoneName = document.getElementById('phoneName');
 const phoneListItem = document.querySelector(".phonelistitem");
 
 inputData.addEventListener('keyup', searchPhone);
+
+
 async function searchPhone(e) {
-    let response = await fetch(
-      "https://api-mobilespecs.azharimm.site/v2/brands/apple-phones-48?page=2"
-    );
+    let response = await fetch(`https://api-mobilespecs.azharimm.site/v2/search?query=${inputData.value}`);
     let data = await response.json();
-    console.log(data)
-    for (let phoneModel of data.brand) {
-        modelHTML = document.createElement('li');
+    // console.log(data)
 
-        // create div with class name phone
-
-        let phoneDiv = document.createElement('div');
-        phoneDiv.classList.add('phone')
-
-        //create div with class name brand
-
-        let phoneBrand = document.createElement('div')
-        phoneBrand.classList.add('brand');
-        phoneBrand.innerHTML =`${phoneModel.brand}`
+    while (phoneListItem.firstChild) {
+        phoneListItem.removeChild(phoneListItem.firstChild)
+    }
 
 
-        phoneDiv.appendChild(phoneBrand)
+    if (data.status) {
+    
+        for (let phone of data.data.phones) {
+            modelHTML = document.createElement('li');
+
+            // create div with class name phone
+
+            let phoneDiv = document.createElement('div');
+            phoneDiv.classList.add('phone')
+
+            //create div with class name brand
+
+            let phoneBrand = document.createElement('div')
+            phoneBrand.classList.add('brand');
+            phoneBrand.innerHTML = `${phone.phone_name}`;
+
+            let phoneImageDiv = document.createElement('div');
+            phoneImageDiv.classList.add('picture');
+
+            let phoneImage = document.createElement('img');
+            phoneImage.setAttribute('src', phone.image)
+            phoneImageDiv.appendChild(phoneImage);
+            phoneDiv.appendChild(phoneImageDiv)
+
+
+
+
+
+
+
+            phoneDiv.appendChild(phoneBrand);
+            modelHTML.appendChild(phoneDiv);
+            phoneListItem.appendChild(modelHTML)
+        }
+    } else {
+        modelHTML = document.createElement('li')
+        modelHTML.innerHTML = 'Results not found'
         phoneListItem.appendChild(modelHTML)
-         
+        
      }
     
 }
-searchPhone()
