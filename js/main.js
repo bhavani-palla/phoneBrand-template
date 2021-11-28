641f38f298bea0aa2ee3cd7d182170345824abb8
Bhavani Palla <bhavani.palla@gmail.com>
2021-11-28 18:44:18 +0100

    Added the event functionality to button

--- js/main.js
+++ js/main.js
@@ -1,73 +1,71 @@
-const inputData = document.getElementById('inputdata');
-const inputPhoneName = document.getElementById('phoneName');
+const inputData = document.getElementById("inputdata");
+const inputPhoneName = document.getElementById("phoneName");
 const phoneListItem = document.querySelector(".phonelistitem");
 
-inputData.addEventListener('keyup', searchPhone);
+inputData.addEventListener("keyup", searchPhone);
 
+async function showDetails(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}
 
 async function searchPhone(e) {
-    let response = await fetch(`https://api-mobilespecs.azharimm.site/v2/search?query=${inputData.value}`);
+  try {
+    let response = await fetch(
+      `https://api-mobilespecs.azharimm.site/v2/search?query=${inputData.value}`
+    );
     let data = await response.json();
     // console.log(data)
 
     while (phoneListItem.firstChild) {
-        phoneListItem.removeChild(phoneListItem.firstChild)
+      phoneListItem.removeChild(phoneListItem.firstChild);
     }
 
-
     if (data.status) {
-    
-        for (let phone of data.data.phones) {
-          modelHTML = document.createElement("li");
+      for (let phone of data.data.phones) {
+        modelHTML = document.createElement("li");
 
-          // create div with class name phone
+        // create div with class name phone
 
-          let phoneDiv = document.createElement("div");
-          phoneDiv.classList.add("phone");
+        let phoneDiv = document.createElement("div");
+        phoneDiv.classList.add("phone");
 
-          //create div with class name brand
+        //create div with class name brand
 
-          let phoneBrand = document.createElement("div");
-          phoneBrand.classList.add("brand");
-          phoneBrand.innerHTML = `${phone.phone_name}`;
+        let phoneBrand = document.createElement("div");
+        phoneBrand.classList.add("brand");
+        phoneBrand.innerHTML = `${phone.phone_name}`;
 
+        let phoneImageDiv = document.createElement("div");
+        phoneImageDiv.classList.add("picture");
 
-          let phoneImageDiv = document.createElement("div");
-          phoneImageDiv.classList.add("picture");
+        let phoneImage = document.createElement("img");
+        phoneImage.setAttribute("src", phone.image);
+        phoneImage.classList.add("phone-pic");
+        phoneImageDiv.appendChild(phoneImage);
+        phoneDiv.appendChild(phoneImageDiv);
 
-          let phoneImage = document.createElement("img");
-          phoneImage.setAttribute("src", phone.image);
-          phoneImageDiv.appendChild(phoneImage);
-          phoneDiv.appendChild(phoneImageDiv);
+        phoneDiv.appendChild(phoneBrand);
+        modelHTML.appendChild(phoneDiv);
+        phoneListItem.appendChild(modelHTML);
 
-          phoneDiv.appendChild(phoneBrand);
-          modelHTML.appendChild(phoneDiv);
-          phoneListItem.appendChild(modelHTML);
+        // create element btn with class name
+        let detailBtn = document.createElement("button");
+        detailBtn.classList.add("details");
+        detailBtn.innerHTML = "Details";
+        detailBtn.setAttribute("onclick", `showDetails("${phone.detail}")`);
+        detailBtn.setAttribute("data-toggle", "modal");
+        detailBtn.setAttribute("data-target", "#myModal");
 
-         // create element btn with class name
-        //   let detailbtn = document.createElement('button');
-        //     detailbtn.classList.add('details');
-        //     detailbtn.innerHTML = `${phone.details}`;
+        phoneDiv.appendChild(detailBtn);
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    // modelHTML = document.createElement("li");
 
-            
-        //     phoneDiv.appendChild(detailbtn);
-            document.getElementById('btn').onclick=function(){
-                let a = document.createElement('a');
-                a.target = '_blank';
-                a.href=''
-
-            }
-
-           
-         
-        }
-                    
-
-    } else {
-        modelHTML = document.createElement('li')
-        modelHTML.innerHTML = 'Results not found'
-        phoneListItem.appendChild(modelHTML)
-        
-     }
-    
+    modelHTML.innerHTML = "Results not found";
+     phoneListItem.appendChild(modelHTML);
+  }
 }
