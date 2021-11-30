62f83816d5ec248ba20a208ef279b77533b6251a
Bhavani Palla <bhavani.palla@gmail.com>
2021-11-30 14:46:26 +0100

    Added styles and events

--- js/main.js
+++ js/main.js
@@ -43,51 +43,56 @@ async function searchPhone(e) {
     }
 
     if (data.status) {
-      for (let phone of data.data.phones) {
-        modelHTML = document.createElement("li");
 
-        // create div with class name phone
 
-        let phoneDiv = document.createElement("div");
-        phoneDiv.classList.add("phone");
+      const phones = data.data.phones;
 
-        //create div with class name brand
+      // if (phones || phones.length < 1) {
+      //   modelHTML = document.createElement("li");
+      //   modelHTML.innerHTML = "Results not found";
+      //   phoneListItem.appendChild(modelHTML);
+      // }
+        for (let phone of phones) {
+          modelHTML = document.createElement("li");
 
-        let phoneBrand = document.createElement("div");
-        phoneBrand.classList.add("brand");
-        phoneBrand.innerHTML = `${phone.phone_name}`;
+          // create div with class name phone
 
-        let phoneImageDiv = document.createElement("div");
-        phoneImageDiv.classList.add("picture");
+          let phoneDiv = document.createElement("div");
+          phoneDiv.classList.add("phone");
 
-        let phoneImage = document.createElement("img");
-        phoneImage.setAttribute("src", phone.image);
-        phoneImage.classList.add("phone-pic");
-        phoneImageDiv.appendChild(phoneImage);
-        phoneDiv.appendChild(phoneImageDiv);
+          //create div with class name brand
 
-        phoneDiv.appendChild(phoneBrand);
-        modelHTML.appendChild(phoneDiv);
-        phoneListItem.appendChild(modelHTML);
+          let phoneBrand = document.createElement("div");
+          phoneBrand.classList.add("brand");
+          phoneBrand.innerHTML = `${phone.phone_name}`;
 
-        // create element btn with class name
-        let detailBtn = document.createElement("button");
-        detailBtn.classList.add("details");
-        detailBtn.innerHTML = "Details";
-        detailBtn.setAttribute("onclick", `showDetails("${phone.detail}")`);
+          let phoneImageDiv = document.createElement("div");
+          phoneImageDiv.classList.add("picture");
 
-        
-         detailBtn.setAttribute("data-toggle", "modal");
-         detailBtn.setAttribute("data-target", "#myModal");
+          let phoneImage = document.createElement("img");
+          phoneImage.setAttribute("src", phone.image);
+          phoneImage.classList.add("phone-pic");
+          phoneImageDiv.appendChild(phoneImage);
+          phoneDiv.appendChild(phoneImageDiv);
 
-        phoneDiv.appendChild(detailBtn);
-      }
+          phoneDiv.appendChild(phoneBrand);
+          modelHTML.appendChild(phoneDiv);
+          phoneListItem.appendChild(modelHTML);
+
+          // create element btn with class name
+          let detailBtn = document.createElement("button");
+          detailBtn.classList.add("details");
+          detailBtn.innerHTML = "Details";
+          detailBtn.setAttribute("onclick", `showDetails("${phone.detail}")`);
+
+          detailBtn.setAttribute("data-toggle", "modal");
+          detailBtn.setAttribute("data-target", "#myModal");
+
+          phoneDiv.appendChild(detailBtn);
+        }
     }
   } catch (error) {
     console.log(error)
-     modelHTML = document.createElement("li");
-
-    modelHTML.innerHTML = "Results not found";
-     phoneListItem.appendChild(modelHTML);
+    
   }
 }
